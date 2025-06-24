14a591f3625d79c7066c1d3d8175de3ea051c1be
Mehdi Asadi <mehdiasadi63@gmail.com>
2025-06-24 18:42:56 +0330

    update page

--- src/app/auth/SendOTPFrom.jsx
+++ src/app/auth/SendOTPFrom.jsx
@@ -17,7 +17,7 @@ function SendOTPFrom({ phoneNumber, onChange, onSubmit, isLoading }) {
 					) : (
 						<button
 							type="submit"
-							className="font-bold rounded-2xl transition-all duration-300 px-4 py-3 bg-blue-500 text-white hover:bg-blue-300 shadow-lg 
+							className="btn-primary font-bold rounded-2xl transition-all duration-300 px-4 py-3 text-white shadow-lg 
 							shadow-blue-800 w-full"
 						>
 							ارسال کد تایید
