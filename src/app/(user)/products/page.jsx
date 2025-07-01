f9763afc2766e5a11da6ca5e35b6102d6adc91ad
Mehdi Asadi <mehdiasadi63@gmail.com>
2025-07-01 21:28:52 +0330

    update page

--- src/app/(user)/products/page.jsx
+++ src/app/(user)/products/page.jsx
@@ -1,5 +1,50 @@
-function Products() {
-	return <div>Products</div>
+import { getCategories } from '@/services/categoryService'
+import { getProducts } from '@/services/productService'
+import Link from 'next/link'
+
+async function Products() {
+	const { products } = await getProducts()
+	const { categories } = await getCategories()
+	return (
+		<div>
+			<h1 className="text-xl font-bold mb-6">صفحه محصولات</h1>
+			<div className="grid grid-cols-4">
+				<ul>
+					{categories.map(category => {
+						return <li key={category._id}>{category.title}</li>
+					})}
+				</ul>
+			</div>
+			<div className="col-span-3 grid grid-cols-3 gap-4">
+				{products.map(product => {
+					return (
+						<div
+							className="col-span-1 border rounded-xl shadow-md p-4"
+							key={product._id}
+						>
+							<h2 className="font-bold text-xl mb-4">
+								{product.title}
+							</h2>
+							<div className="mb-4">
+								<span>تاریخ ساختن: </span>
+								<span className="font-bold">
+									{toLocalDateStringShort(product.createdAt)}
+								</span>
+							</div>
+							<Link
+								className="text-primary-900 font-bold mb-4 block"
+								href={`/products/${product.slug}`}
+							>
+								مشاهده محصول
+							</Link>
+							{/* <LikeProduct product={product} /> */}
+							{/* <AddToCart product={product} /> */}
+						</div>
+					)
+				})}
+			</div>
+		</div>
+	)
 }
 
 export default Products
