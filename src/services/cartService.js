221f39c91916a2cbf0215a98110abd11162e7dd4
Mehdi Asadi <mehdiasadi63@gmail.com>
2025-07-17 17:16:06 +0330

    add cartService

---
+++ src/services/cartService.js
@@ -0,0 +1,7 @@
+import http from './httpService'
+
+export function addToCart(productId) {
+	return http
+		.post('/cart/add', { productId })
+		.then(({ data }) => data.data)
+}
--- src/services/productService.js
+++ src/services/productService.js
@@ -3,3 +3,9 @@ import http from './httpService'
 export function getProducts() {
 	return http.get('/product/list').then(({ data }) => data.data)
 }
+
+export function getOneProductBySlug(slug) {
+	return http
+		.get(`/product/slug/${slug}`)
+		.then(({ data }) => data.data)
+}
