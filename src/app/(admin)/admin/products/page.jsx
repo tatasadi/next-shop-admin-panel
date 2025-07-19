63710e2b2bfe197d12a25a4eb89406c78ff8a77f
Mehdi Asadi <mehdiasadi63@gmail.com>
2025-07-19 17:24:32 +0330

    add productAdmin

---
+++ src/app/(admin)/admin/products/page.jsx
@@ -0,0 +1,37 @@
+'use client'
+
+import { useGetProducts } from '@/hooks/useProducts'
+import ProductListTable from './ProductListTable'
+import Link from 'next/link'
+import { HiPlusCircle } from 'react-icons/hi'
+import Loader from '@/ui/Loader'
+
+function page() {
+	const { data, isLoading } = useGetProducts()
+	const { products } = data || {}
+
+	if (isLoading) return <Loader />
+
+	return (
+		<div>
+			<div className="mb-5 flex items-center justify-between">
+				<h1 className="text-xl font-bold mb-5">محصولات</h1>
+				<Link
+					href="/admin/products/add"
+					className="font-bold text-primary-900 flex items-center gap-x-2"
+				>
+					<HiPlusCircle className="w-6 h-6" />{' '}
+					<span>اضافه کردن محصول</span>
+				</Link>
+			</div>
+			<ProductListTable products={products} />
+		</div>
+	)
+}
+export default page
+
+// add
+// edit (update)
+// remove (delete)
+//  /admin/products/add (add)
+// /admin/products/active()
--- src/services/productService.js
+++ src/services/productService.js
@@ -9,3 +9,31 @@ export function getOneProductBySlug(slug) {
 		.get(`/product/slug/${slug}`)
 		.then(({ data }) => data.data)
 }
+export function getOneProductById(id) {
+	return http.get(`/product/${id}`).then(({ data }) => data.data)
+}
+export function likeProduct(id) {
+	return http
+		.post(`/product/like/${id}`)
+		.then(({ data }) => data.data)
+}
+
+// admin relate function
+export function addProduct(data) {
+	return http
+		.post(`/admin/product/add`, data)
+		.then(({ data }) => data.data)
+}
+
+export function removeProduct(id) {
+	return http
+		.delete(`/admin/product/remove/${id}`)
+		.then(({ data }) => data.data)
+}
+
+export function updateProduct({ productId, data }) {
+	console.log({ data })
+	return http
+		.patch(`/admin/product/update/${productId}`, data)
+		.then(({ data }) => data.data)
+}
