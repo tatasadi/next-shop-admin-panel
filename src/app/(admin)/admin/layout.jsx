11adf7eb6d97e5473734b7d2235a7ea46c5eb0a2
Mehdi Asadi <mehdiasadi63@gmail.com>
2025-07-19 12:30:37 +0330

    add admin

---
+++ src/app/(admin)/admin/layout.jsx
@@ -0,0 +1,33 @@
+import vazirFont from '@/constants/localFonts'
+import '../../globals.css'
+import Providers from '@/pages/Providers'
+import { Toaster } from 'react-hot-toast'
+import AdminSideBar from './AdminSideBar'
+
+export const metadata = {
+	title: 'پروفایل ادمین',
+	description: 'پروفایل ادمین',
+}
+
+export default function RootLayout({ children }) {
+	return (
+		<html lang="fa" dir="rtl">
+			<body
+				suppressHydrationWarning={true}
+				className={`${vazirFont.variable} font-sans`}
+			>
+				<Providers>
+					<Toaster />
+					<div className="grid grid-cols-5 bg-white h-screen">
+						<div className="col-span-1 bg-gray-100 overflow-y-auto p-4">
+							<AdminSideBar />
+						</div>
+						<div className="col-span-4 overflow-y-auto p-4">
+							{children}
+						</div>
+					</div>
+				</Providers>
+			</body>
+		</html>
+	)
+}
